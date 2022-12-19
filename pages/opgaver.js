0ff8e728e5eb694d611d2b47034f92a360cea515
AdvancingHermit <christiannkya@outlook.com>
2022-12-19 15:15:11 +0100

    Kan Indsætte brugere fra frontend

---
+++ pages/opgaver.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Opgaver() {
+  return (
+    <>
+      <h1>Lav opgaver her</h1>
+      <h2>
+        <Link href="/">Gå tilbage til hovedmenu</Link>
+      </h2>
+      make a form with email and password
+
+    </>
+  );
+}
