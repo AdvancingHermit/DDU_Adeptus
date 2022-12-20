aa7d8b0fc1d5de1e0119113766d319ff76703778
AdvancingHermit <christiannkya@outlook.com>
2022-12-20 11:32:52 +0100

    Kan lave opgaver o gkan besvare opgaver

--- pages/opgaver.js
+++ pages/opgaver.js
@@ -1,45 +1,14 @@
 import Link from 'next/link';
 
-export default function Assignments() {
+export default function Opgaver() {
+  return (
+    <>
+      <h1>Lav opgaver her</h1>
+      <h2>
+        <Link href="/">Gå tilbage til hovedmenu</Link>
+      </h2>
+      make a form with email and password
 
-    const createInput = async () => {
-        const atInput = document.getElementById("assignmentText").value;
-        const caInput = document.getElementById("correctAnswer").value;
-        const res = await fetch("/api/addSingle", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                assignmentText: atInput,
-                correctAnswer: caInput,
-            }),
-        });
-        const data = await res.json();
-        console.log(data);
-    };
-
-    return (
-        <>
-            <h1>Lav opgaver her</h1>
-            <h2>
-                <Link href="/">Gå tilbage til hovedmenu</Link>
-            </h2>
-
-            <form>
-                <label>
-                    Spørgsmål:
-                    <input type="text" name="assignmentText" id="assignmentText" />
-                </label>
-                <br/>
-                <label>
-                    Svar:
-                    <input type="text" name="correctAnswer" id="correctAnswer" />
-                </label>
-            </form>
-            <br/>
-            <button onClick={createInput}>Create Assignment With Input</button>
-
-        </>
-    );
+    </>
+  );
 }
